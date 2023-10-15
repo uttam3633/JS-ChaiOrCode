0ee2b0a9d00a5d1d55c80009e3b78ca1d1f47486
uttam3633 <93640349+uttam3633@users.noreply.github.com>
2023-10-15 17:26:57 +0000

    ComaprsionCheck

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// learn about type script
+
+// console.log(null >0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+//The reason is that an equality check == and comparisons > < > = < = work differently
+// Comparisons convert null to a number, treating it ass 0.
+// that's why (3) null >=0 is true and (1) null >0 is false
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === (strick check)
+
+console.log("2" === 2);
