6fba3c9a9afd12a63ca50abd6d5b21169a9517a3
uttam3633 <93640349+uttam3633@users.noreply.github.com>
2023-10-15 05:34:24 +0000

    dataTypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict" // treat all JS code as newer version
+//alert(3+3) // we are using node JS, not browser
+
+
+console.log(3
+    +
+    3);
+     //readablity matter
+
+
+console.log("Uttam");
+
+
+let name="uttam"
+let age= 18
+let isLoggedIn =false
+
+// number => 2 ^ 53
+// bigInt
+// string => "";''
+// bollean 
+// null => standalone value
+// Undefine => value not assigned
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+console.log(typeof age);
