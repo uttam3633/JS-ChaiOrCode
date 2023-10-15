26fab9a39abce9e987a4e77df1b70e6508d8e49c
uttam3633 <93640349+uttam3633@users.noreply.github.com>
2023-10-15 05:12:14 +0000

    new file:   01_basics/01_variable.js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountID =123456
+let accountEmail ="Uttamkumar1865@gmail.com"
+var accountPassword = "87454666656465"
+accountCity="Jaipur"
+let accountState
+
+
+//accountID = 1234 // not allowed 
+accountEmail="hchc@google.com"
+accountPassword="121212121212"
+accountCity = "Bengal"
+
+/*
+prefer not to use Var
+because of issue in block scope and functional scope 
+*/
+console.log(accountID);
+
+console.table([accountID, accountEmail,accountPassword,accountCity,accountState])
+
